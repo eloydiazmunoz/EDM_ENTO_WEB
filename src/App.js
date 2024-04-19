58b1488e07755ad75b70a96d005480387a955103
eloy <eloydiazlsgi@gmail.com>
2024-04-19 10:18:51 +0200

    add react

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Test from './Test/test.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Test/>
         <p>
           Edit <code>src/App.js</code> and save to reload.ELOY 
         </p>
