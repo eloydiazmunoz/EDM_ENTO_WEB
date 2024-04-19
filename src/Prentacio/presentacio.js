7c171abc495c3d0dc8c4d370ca4ef4ff47f29315
eloy <eloydiazlsgi@gmail.com>
2024-04-19 10:41:40 +0200

    add react

--- src/Prentacio/presentacio.js
+++ src/Prentacio/presentacio.js
@@ -3,8 +3,8 @@ import captura from './Captura.png';
 function Presentacio() {
     return (
       <div className="">
-        <img src={captura} alt="imagen"/> 
         <p>Sóc un professional introspectiu amb passió pel treball en equip. Motivat pels reptes tecnològics en psicologia clínica,sóc autodidacta, versàtil i proactiu, sempre amb l'objectiu d'excel·lir professionalment.</p>
+        <img src={captura} alt="imagen"/> 
       </div>
     )
   }
