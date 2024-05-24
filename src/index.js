c19224aa48da9d563c343f70d8a112ec29b2dcf7
eloy <eloydiazlsgi@gmail.com>
2024-05-24 09:07:09 +0200

    add react

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from "./App";
 import About from "./about/About";
 import Hobbies from "./hobbies";
 import NavigationMenu from "./Menu/NavigationMenu.js";
+import linkedinIcon from "./linkedin.png"; // Asegúrate de tener este icono en tu proyecto
 
 // Rutas definidas para tu aplicación
 const router = createBrowserRouter([
@@ -27,12 +28,34 @@ const router = createBrowserRouter([
   },
 ]);
 
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="social-media">
+          <a
+            href="https://www.linkedin.com/in/eloy-diaz-mu%C3%B1oz-87b26023b/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={linkedinIcon} alt="LinkedIn" className="social-icon" />
+          </a>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2024 Eloy Diaz Muñoz. Tots els drets reservats.</p>
+      </div>
+    </footer>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <div>
       <NavigationMenu />
       <RouterProvider router={router} />
+      <Footer />
     </div>
   </React.StrictMode>
 );
